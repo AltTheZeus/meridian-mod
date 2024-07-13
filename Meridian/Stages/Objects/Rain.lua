fcdae167f7517d416e84b7a4f386a0dcb7404178
Altzeus <88247068+AltTheZeus@users.noreply.github.com>
2024-07-14 01:43:21 +0300

    Add files via upload

---
+++ Meridian/Stages/Objects/Rain.lua
@@ -0,0 +1,13 @@
+local RainP = ParticleType.find("Rain")
+local RotColor = Color.fromHex(0x6C754d)
+
+local RainGreen = Object.new("RainGreen")
+
+RainGreen:addCallback("step", function(self)
+    RainP:burst("above", camera.x + math.random(1920, 0), camera.y, 1, RotColor)
+    RainP:burst("above", camera.x + math.random(1920, 0), camera.y, 1, RotColor)
+    RainP:burst("above", camera.x + math.random(1920, 0), camera.y, 1, RotColor)
+    RainP:burst("above", camera.x + math.random(1920, 0), camera.y, 1, RotColor)
+end)
+
+
