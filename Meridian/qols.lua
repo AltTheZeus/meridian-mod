a6120fcb58cae20ec289fba4f0c6d12fa11572a0
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2024-08-08 15:02:40 +0200

    Add files via upload

---
+++ Meridian/qols.lua
@@ -0,0 +1,12 @@
+--Depth Change so you can actually see the player when infront of an interactable
+obj.Chest1.depth = 5
+obj.Chest2.depth = 5
+obj.Chest3.depth = 5
+obj.Chest4.depth = 5
+obj.Chest5.depth = 5
+
+obj.Shrine1.depth = 5
+obj.Shrine2.depth = 5
+obj.Shrine3.depth = 5
+obj.Shrine4.depth = 5
+obj.Shrine5.depth = 5
