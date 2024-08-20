e8ba0d5c9743d4d680f4cd938dcd5a6a246644e0
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-08-21 07:31:45 +1000

    Add files via upload

---
+++ Meridian/Enemies/enemies.lua
@@ -0,0 +1,14 @@
+local path = "Enemies/"
+
+require(path.."BasaltCrab.basaltCrab")
+require(path.."construct1.con1")
+require(path.."construct2.con2")
+require(path.."guardDog.dog")
+require(path.."stoneGiant.giant")
+
+registercallback("onStageEntry", function()
+	local dD = misc.director:getData()
+	if misc.director:get("stages_passed") >= 6 and dD.PLstages == false then
+		dD.PLstages = true
+	end
+end)
