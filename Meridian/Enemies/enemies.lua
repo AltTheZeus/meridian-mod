7fb0f6fa0b078160857bb462422c54010b22d058
Altzeus <88247068+AltTheZeus@users.noreply.github.com>
2024-08-21 14:11:31 +0300

    Add files via upload

--- Meridian/Enemies/enemies.lua
+++ Meridian/Enemies/enemies.lua
@@ -1,10 +1,14 @@
 local path = "Enemies/"
 
+-- enemies
 require(path.."BasaltCrab.basaltCrab")
 require(path.."construct1.con1")
 require(path.."construct2.con2")
 require(path.."guardDog.dog")
 require(path.."stoneGiant.giant")
+
+-- bosses
+require(path.."Lacertian.lacertian")
 
 registercallback("onStageEntry", function()
 	local dD = misc.director:getData()
