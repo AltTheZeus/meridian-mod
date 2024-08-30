82f23c9b568ebb3ca0019d2814058e7f427ec02f
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-08-30 17:21:03 +1000

    Update elites.lua
    winslow you didnt even remove it from here.........

--- Meridian/Elites/elites.lua
+++ Meridian/Elites/elites.lua
@@ -5,5 +5,7 @@ require(path.."forsaken")
 require(path.."bubble")
 require(path.."sorrow")
 require(path.."evoke")
-require(path.."gildedLeaf")
+if not modloader.checkFlag("mn_disable_items") then 
+	require("Items/gildedLeaf")
+end
 require(path.."blessedTree")
