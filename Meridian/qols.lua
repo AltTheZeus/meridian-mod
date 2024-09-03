ab7002d30641392dc2e1e45fef873436738f10ea
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2024-09-03 16:29:01 +0200

    Update qols.lua

--- Meridian/qols.lua
+++ Meridian/qols.lua
@@ -10,3 +10,6 @@ obj.Shrine2.depth = 5
 obj.Shrine3.depth = 5
 obj.Shrine4.depth = 5
 obj.Shrine5.depth = 5
+
+Sprite.find("Water1"):replace(Sprite.load("Stages/vanilla/Water1", 1, 0, 0))
+Sprite.find("Water2"):replace(Sprite.load("Stages/vanilla/Water2", 1, 0, 0))
