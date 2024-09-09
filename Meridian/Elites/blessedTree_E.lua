efcec1b2bc4091522ee80c673b11d172582fe85d
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-10 09:41:18 +1000

    Add files via upload
    auric tree resprite

--- Meridian/Elites/blessedTree_E.lua
+++ Meridian/Elites/blessedTree_E.lua
@@ -1,27 +1,27 @@
-tree = Object.base("Enemy", "Auric Tree")
-local treeIdle = Sprite.load("Elites/auricIdle", 1, 11, 27)
-local treeSpawn = Sprite.load("Elites/auricSpawn", 3, 11, 27)
-local treeMask = Sprite.load("Elites/auricMask", 1, 11, 27)
-local treeDeath = Sprite.load("Elites/auricDeath", 7, 16, 33)
-local shing = Sound.load("Elites/tree.ogg")
-local gogobalga = Sound.load("Elites/treeDeath.ogg")
+treeE = Object.base("Enemy", "Auric Tree (evil)")
+local treeIdle_E = Sprite.load("Elites/auricIdle_E", 8, 17, 48)
+local treeSpawn_E = Sprite.load("Elites/auricSpawn_E", 14, 44, 77)
+local treeMask_E = Sprite.load("Elites/auricMask_E", 1, 17, 48)
+local treeDeath_E = Sprite.load("Elites/auricDeath_E", 8, 29, 52)
+local shing = Sound.find("tree")
+local gogobalga = Sound.find("treeDeath")
 if not modloader.checkFlag("mn_disable_items") then
 	local aspect = Item.find("Gilded Leaf")
 end
 
-treeDead = Object.new("tree but its DEAD")
-treeDead:addCallback("create", function(self)
+treeDeadE = Object.new("tree but its DEAD (still evil)")
+treeDeadE:addCallback("create", function(self)
 	local data = self:getData()
 	data.timer = 0
 	data.timerActive = 0
-	self.sprite = treeDeath
+	self.sprite = treeDeath_E
 	self.spriteSpeed = 0.2
 	data.age = 0
 end)
-treeDead:addCallback("step", function(self)
+treeDeadE:addCallback("step", function(self)
 	local iD = self:getData()
 	print(self.subimage)
-	if iD.timerActive == 0 and self.subimage >= 7 then
+	if iD.timerActive == 0 and self.subimage >= 8 then
 		iD.timerActive = 1
 	end
 	if iD.timerActive == 1 then
@@ -32,40 +32,31 @@ treeDead:addCallback("step", function(self)
 	end
 end)
 
-treeSpawner = Object.new("tree spawner")
-treeSpawner:addCallback("create", function(self)
+treeSpawnerE = Object.new("tree spawner (sinister)")
+treeSpawnerE:addCallback("create", function(self)
 	local data = self:getData()
 	data.timer = 0
 end)
-treeSpawner:addCallback("step", function(self)
+treeSpawnerE:addCallback("step", function(self)
 	local data = self:getData()
 	data.timer = data.timer + 1
 	if data.timer >= 60 then
-		local ad = tree:create(self.x, self.y):getData()
+		local ad = treeE:create(self.x, self.y):getData()
 		ad.isPlayer = 0
 		self:destroy()
 	end
 end)
 
-local leaf = Sprite.load("Elites/leafEf", 1, 2, 2)
-local leafEF = ParticleType.new("auricLeaf")
-	leafEF:sprite(leaf, false, false, false)
-	leafEF:scale(1, -1)
-	leafEF:alpha(1, 0.75, 0)
-	leafEF:angle(-20, 20, 0, 0, true)
-	leafEF:speed(-1, 1, -0.02, 0.02)
-	leafEF:direction(0, 0, 0, 0)
-	leafEF:gravity(0.08, 270)
-	leafEF:life(60 * 1, 60 * 1.5)
+local leafEF = ParticleType.find("auricLeaf")
 
-tree:addCallback("create", function(self)
+treeE:addCallback("create", function(self)
 	shing:play()
 	local data = self:getData()
 	local ac = self:getAccessor()
 	data.efTimer = 0
 	data.timer = 0
 	data.timerActive = 0
-	self.sprite = treeSpawn
+	self.sprite = treeSpawn_E
 	self.spriteSpeed = 0.2
 	ac.pHmax = 0
 	ac.can_jump = 0
@@ -79,10 +70,10 @@ tree:addCallback("create", function(self)
 	ac.point_value = 0
 	ac.state = "idle"
 	data.age = 0
-	self.mask = treeMask
+	self.mask = treeMask_E
 	self:setAnimations{
-		idle = treeIdle,
-		jump = treeIdle
+		idle = treeIdle_E,
+		jump = treeIdle_E
 	}
 	if not self:collidesMap(self.x, self.y) then
 		local UHOHcheck = 0
@@ -112,10 +103,10 @@ tree:addCallback("create", function(self)
 	until leafCount == 8
 end)
 
-tree:addCallback("destroy", function(self)
+treeE:addCallback("destroy", function(self)
 	local leafCount = 0
 	gogobalga:play()
-	treeDead:create(self.x, self.y)
+	treeDeadE:create(self.x, self.y)
 	if not modloader.checkFlag("mn_disable_items") then
 	if math.random(1,4000) == 1 then
 		aspect:create(self.x, self.y - 15)
@@ -142,65 +133,29 @@ end)
 
 local circle = Object.find("efCircle")
 local enemies = ParentObject.find("enemies")
-local treegen = Buff.new("Auric Blessing")
-	treegen.sprite = Sprite.load("Elites/treeHP", 1, 10, 0)
-local treeArmor = Buff.new("Auric Distinction")
-	treeArmor.sprite = Sprite.load("Elites/treeShield", 1, 10, 0)
+local treegen = Buff.find("Auric Blessing")
+local treeArmor = Buff.find("Auric Distinction")
 
-treegen:addCallback("start", function(guy)
-	local gA = guy:getAccessor()
-	local gD = guy:getData()
-	gD.treeBuff1 = gA.maxhp / 600
-	gA.hp_regen = gA.hp_regen + gD.treeBuff1
-end)
-
-treegen:addCallback("step", function(guy)
-	local gA = guy:getAccessor()
-	local gD = guy:getData()
-	if guy:getObject() == Object.find("p") then
-	else
-		gA.hp = gA.hp + gD.treeBuff1
-		if gA.hp > gA.maxhp then gA.hp = gA.maxhp end
-	end
-end)
-
-treegen:addCallback("end", function(guy)
-	local gA = guy:getAccessor()
-	local gD = guy:getData()
-	gA.hp_regen = gA.hp_regen - gD.treeBuff1
-	gD.treeBuff1 = 0
-end)
-
-treeArmor:addCallback("start", function(guy)
-	local gA = guy:getAccessor()
-	gA.armor = gA.armor + 25
-end)
-
-treeArmor:addCallback("end", function(guy)
-	local gA = guy:getAccessor()
-	gA.armor = gA.armor - 25
-end)
-
-tree:addCallback("step", function(self)
+treeE:addCallback("step", function(self)
 	local iD = self:getData()
-	if iD.timerActive == 0 and self.sprite == treeSpawn and self.subimage >= 3 then
+	if iD.timerActive == 0 and self.sprite == treeSpawn_E and self.subimage >= 14 then
 		iD.timerActive = 1
 	end
 	if iD.timerActive == 1 then
 		iD.timer = iD.timer + 1
 		if iD.timer >= 5 then
-			self.sprite = treeIdle
+			self.sprite = treeIdle_E
 		end
 	end
 	iD.efTimer = iD.efTimer + 1
 	if iD.efTimer >= 90 then
 		local eehee = circle:create(self.x - 2, self.y - 5)
 		local eD = eehee:getAccessor()
-		eD.radius = 40
+		eD.radius = 65
 		eehee.blendColor = Color.fromRGB(255, 237, 187)
 		iD.efTimer = 0
 		if iD.isPlayer == 0 then
-			for _, a in ipairs(ParentObject.find("actors"):findAllEllipse(self.x - 2 - 61, self.y - 5 - 61, self.x - 2 + 61, self.y - 5 + 61)) do
+			for _, a in ipairs(ParentObject.find("actors"):findAllEllipse(self.x - 2 - 87, self.y - 5 - 87, self.x - 2 + 87, self.y - 5 + 87)) do
 				if a:getObject() == tree then
 				elseif a:getAccessor().team == "enemy" then
 					a:applyBuff(treeArmor, 95)
@@ -225,3 +180,10 @@ tree:addCallback("step", function(self)
 end)
 
 local bID = EliteType.find("blessed").ID
+
+registercallback("onNPCDeath", function(npc)
+	local nD = npc:getData()
+	if npc:get("elite_type") == bID then
+		treeSpawnerE:create(npc.x, npc.y - 3)
+	end
+end)
