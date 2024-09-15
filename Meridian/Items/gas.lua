70b94c94e1ca559b29a7672f3a82e1df6002aa78
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-15 21:08:43 +1000

    Update gas.lua
    fix sprite alignment

--- Meridian/Items/gas.lua
+++ Meridian/Items/gas.lua
@@ -2,7 +2,7 @@ local item = Item("Tear Gas")
 
 item.pickupText = "Increase damage against stunned enemies." 
 
-item.sprite = Sprite.load("Items/gas.png", 1, 12,  13)
+item.sprite = Sprite.load("Items/gas.png", 1, 15, 15)
 
 registercallback("onFireSetProcs", function(damager, parent)
 	if parent:getObject() == Object.find("p") then
