7e92aaa1bf65cf34568bec64fed4332ccaf06430
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-18 19:46:13 +1000

    Beta Construct 2 changes
    - Beta Construct can now aim at airborne players more effectively
    - fixed beta construct 2 portrait

--- Meridian/Enemies/construct2/con2.lua
+++ Meridian/Enemies/construct2/con2.lua
@@ -1,8 +1,125 @@
 local path = "Enemies/construct2/"
+
+local bulletCheck = Object.new("bCon2Bullet")
+bulletCheck.sprite = Sprite.load("bConBullet", path.."bConBullet", 1, 0, 9)
+
+local headSprites = {
+    idle = Sprite.load("con2Idle_B", path.."con2Idle_B", 1, 8, 6),
+    walk = Sprite.load("con2Walk_B", path.."con2Walk_B", 6, 8, 13),
+    shoot = Sprite.load("con2Shoot_B", path.."con2Shoot_B", 12, 8, 11),
+    palette = Sprite.load("con2Pal_B", path.."con2Pal_B", 1, 0, 0),
+    jump = Sprite.load("con2Jump_B", path.."con2Jump_B", 1, 8, 6),
+    death = Sprite.load("con2Death_B", path.."con2Death_B", 9, 11, 6),
+    spawn = Sprite.load("con2Spawn_B", path.."con2Spawn_B", 15, 8, 26)
+}
+
+local head = Object.base("Enemy", "Beta Construct Head")
+head.sprite = headSprites.idle
+
+EliteType.registerPalette(headSprites.palette, head)
+
+head:addCallback("create", function(actor)
+    local actorAc = actor:getAccessor()
+    local data = actor:getData()
+    actorAc.name = "Beta Construct"
+--    actorAc.maxhp = data.body:get("maxhp")
+--    actorAc.hp = data.body:get("maxhp")
+--    actorAc.damage = data.body:get("damage")
+    actorAc.pHmax = 0
+    actorAc.walk_speed_coeff = 0
+    actor:setAnimations{
+        idle = headSprites.idle,
+        jump = headSprites.jump,
+        walk = headSprites.walk,
+        shoot = headSprites.shoot,
+        death = headSprites.death,
+        spawn = headSprites.spawn,
+	palette = headSprites.palette
+    }
+--    actorAc.sound_hit = Sound.find("MushHit","vanilla").id
+--    actorAc.sound_death = sounds.death.id
+--    actor.mask = sprites.mask
+    actorAc.health_tier_threshold = 3
+    actorAc.knockback_cap = 6 * Difficulty.getScaling("hp")
+    actorAc.exp_worth = 0
+    actorAc.can_drop = 0
+    actorAc.can_jump = 0
+    data.shot = false
+    data.angleLocked = false
+end)
+
+local everyonelol = ParentObject.find("actors")
+head:addCallback("step", function(self)
+	local sD = self:getData()
+	local actorAc = self:getAccessor()
+	if not sD.body or not sD.body:isValid() then self:destroy() return end
+	if self:get("prefix_type") ~= sD.body:get("prefix_type") then
+		if sD.body:get("prefix_type") == 1 then
+			actorAc.elite_type = (sD.body:get("elite_type"))
+			actorAc.prefix_type = 1
+		elseif sD.body:get("prefix_type") == 2 then
+--			self:makeBlighted(sD.body:getBlighted())
+			actorAc.prefix_type = 2
+		end
+	end
+	actorAc.team = sD.body:get("team")
+	actorAc.maxhp = sD.body:get("maxhp")
+	actorAc.hp = sD.body:get("maxhp")
+	actorAc.damage = sD.body:get("damage")
+	self.sprite = Sprite.find(sD.body.sprite:getName() .. "_B")
+	self.subimage = sD.body.subimage
+	self.xscale = sD.body.xscale
+	self.yscale = sD.body.yscale
+	self.x = sD.body.x + (1 * sD.body.xscale)
+	self.y = sD.body.y - (17 * sD.body.yscale)
+	local targ = Object.findInstance(sD.body:get("target"))
+	if sD.angleLocked == false then
+	if targ ~= nil and targ.y < self.y then
+		local xVar = (math.sign(self.x - targ.x) * (self.x - targ.x))
+		local yVar = (math.sign(self.y - targ.y) * (self.y - targ.y))
+		local c2 = (xVar * xVar) + (yVar * yVar)
+		local h = c2 ^ 0.5
+		local a = xVar
+		self.angle = math.deg(math.asin(a/h))/2 * self.xscale
+--		print(self.angle)
+		if math.abs(self.angle) > 75 then
+			self.angle = 75 * self.xscale
+		end
+	else
+		self.angle = 0
+	end
+	end
+	if self.sprite == headSprites.shoot and self.subimage <= 2 then
+		sD.shot = false
+	elseif self.sprite == headSprites.shoot and self.subimage >= 8 then
+		sD.angleLocked = true
+	end
+	if self.sprite == headSprites.shoot and self.subimage >= 9 and self.subimage <= 10 and sD.shot == false then
+		local explosion = bulletCheck:create(self.x + (9 * self.xscale), self.y - (3 * self.yscale))
+		explosion.mask = explosion.sprite
+		explosion.angle = self.angle
+		explosion.xscale = self.xscale
+		explosion.yscale = self.yscale
+--		explosion.alpha = 0.2
+		explosion:set("visible", 0)
+		for _, i in ipairs(everyonelol:findAll()) do
+			if i:get("team") ~= self:get("team") and explosion:collidesWith(i, explosion.x, explosion.y) then
+				local bullet = sD.body:fireBullet(i.x, i.y, 0, 5, 1)
+				bullet:set("specific_target", i.id)
+			end
+		end
+		sD.shot = true
+	end
+	if self.sprite ~= headSprites.shoot then
+		sD.angleLocked = false
+	end
+end)
+
 local sprites = {
     idle = Sprite.load("con2Idle", path.."con2Idle", 1, 7, 23),
     walk = Sprite.load("con2Walk", path.."con2Walk", 6, 7, 30),
     shoot = Sprite.load("con2Shoot", path.."con2Shoot", 12, 7, 28),
+    log = Sprite.load("con2Shoot_LOG", path.."con2Shoot_LOG", 12, 7, 28),
     spawn = Sprite.load("con2Spawn", path.."con2Spawn", 15, 8, 26),
     death = Sprite.load("con2Death", path.."con2Death", 9, 10, 24),
     mask = Sprite.load("con2Mask", path.."con2Mask", 1, 7, 23),
@@ -47,6 +164,8 @@ con2:addCallback("create", function(actor)
     actorAc.exp_worth = 5 * Difficulty.getScaling()
     actorAc.can_drop = 1
     actorAc.can_jump = 1
+    local mD = head:create(actor.x, actor.y):getData()
+    mD.body = actor
 end)
 
 Monster.giveAI(con2)
@@ -58,7 +177,7 @@ end)
 Monster.skillCallback(con2, 1, function(actor, relevantFrame)
 	if relevantFrame == 9 then
 		sounds.attack:play(1 + 1)
-		actor:fireExplosion(actor.x + actor.xscale * 30, actor.y - 18, 30/19, 5/4, 1)
+--		actor:fireExplosion(actor.x + actor.xscale * 30, actor.y - 18, 30/19, 5/4, 1)
 	end
 end)
 
@@ -79,6 +198,21 @@ card.cost = 8
 for _, elite in ipairs(EliteType.findAll("vanilla")) do
     card.eliteTypes:add(elite)
 end
+
+local headCard = MonsterCard.new("con2Head", head)
+headCard.sprite = sprites.idle
+headCard.sprite = sprites.spawn
+headCard.sound = sounds.spawn
+headCard.canBlight = true
+headCard.type = "classic"
+registercallback("onStageEntry", function()
+	for _, elite in ipairs(headCard.eliteTypes:toTable()) do
+		headCard.eliteTypes:remove(elite)
+	end
+	for _, elite in ipairs(card.eliteTypes:toTable()) do
+		headCard.eliteTypes:add(elite)
+	end
+end, -100)
 
 local stages
 if modloader.checkMod("Starstorm") then
@@ -140,5 +274,5 @@ monsLog.story = "Malformed constructs whizz past me, shaky legs carrying their c
 monsLog.statHP = 70
 monsLog.statDamage = 12
 monsLog.statSpeed = 1.3
-monsLog.sprite = sprites.shoot
+monsLog.sprite = sprites.log
 monsLog.portrait = sprites.portrait
