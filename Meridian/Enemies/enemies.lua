7e748f3f4cf44ac772f528864ba1212dba019d91
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-22 22:04:39 +1000

    Update enemies.lua
    - required dewdrops

--- Meridian/Enemies/enemies.lua
+++ Meridian/Enemies/enemies.lua
@@ -6,6 +6,11 @@ require(path.."construct1.con1")
 require(path.."construct2.con2")
 require(path.."guardDog.dog")
 require(path.."stoneGiant.giant")
+if modloader.checkFlag("enable_mergers") then
+	require(path.."mergers.merger")
+	require(path.."mergers.mergerM")
+	require(path.."mergers.mergerS")
+end
 
 -- bosses
 require(path.."Lacertian.lacertian")
