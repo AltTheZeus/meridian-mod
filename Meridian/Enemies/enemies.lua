5791a2872a09a214703f4acc519f10d5dff126a8
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-28 16:43:54 +1000

    Update enemies.lua
    require doomdrop

--- Meridian/Enemies/enemies.lua
+++ Meridian/Enemies/enemies.lua
@@ -7,6 +7,7 @@ require(path.."construct2.con2")
 require(path.."guardDog.dog")
 require(path.."stoneGiant.giant")
 if modloader.checkFlag("enable_mergers") then
+	require(path.."mergers.mergerG")
 	require(path.."mergers.merger")
 	require(path.."mergers.mergerM")
 	require(path.."mergers.mergerS")
