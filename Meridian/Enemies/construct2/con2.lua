6522b0b0df9c3d59a49bcd872fc6b58e27fefbad
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-09-30 11:17:05 +1000

    head rotation!! finally
    beta construct B heads now track the player correctly - shoutout Assa!!!!!!

--- Meridian/Enemies/construct2/con2.lua
+++ Meridian/Enemies/construct2/con2.lua
@@ -13,22 +13,22 @@ registercallback("postLoad", function()
 	EliteType.refreshPalettes()
 --	print(eliteClones)
 end)]]
-		
+
 
 local path = "Enemies/construct2/"
 
 local bulletCheck = Object.new("bCon2Bullet")
-bulletCheck.sprite = Sprite.load("bConBullet", path.."bConBullet", 1, 0, 7)
+bulletCheck.sprite = Sprite.load("bConBullet", path .. "bConBullet", 1, 0, 7)
 
 local headSprites = {
-    idle = Sprite.load("con2Idle_B", path.."con2Idle_B", 1, 7, 13),
-    walk = Sprite.load("con2Walk_B", path.."con2Walk_B", 6, 7, 20),
-    shoot = Sprite.load("con2Shoot_B", path.."con2Shoot_B", 12, 7, 18),
-    palette = Sprite.load("con2Pal_B", path.."con2Pal_B", 1, 0, 0),
-    jump = Sprite.load("con2Jump_B", path.."con2Jump_B", 1, 7, 14),
-    death = Sprite.load("con2Death_B", path.."con2Death_B", 9, 10, 14),
-    spawn = Sprite.load("con2Spawn_B", path.."con2Spawn_B", 15, 8, 16),
-    mask = Sprite.load("con2Mask_B", path.."con2Mask_B", 1, 0, 0)
+	idle = Sprite.load("con2Idle_B", path .. "con2Idle_B", 1, 7, 13),
+	walk = Sprite.load("con2Walk_B", path .. "con2Walk_B", 6, 7, 20),
+	shoot = Sprite.load("con2Shoot_B", path .. "con2Shoot_B", 12, 7, 18),
+	palette = Sprite.load("con2Pal_B", path .. "con2Pal_B", 1, 0, 0),
+	jump = Sprite.load("con2Jump_B", path .. "con2Jump_B", 1, 7, 14),
+	death = Sprite.load("con2Death_B", path .. "con2Death_B", 9, 10, 14),
+	spawn = Sprite.load("con2Spawn_B", path .. "con2Spawn_B", 15, 8, 16),
+	mask = Sprite.load("con2Mask_B", path .. "con2Mask_B", 1, 0, 0)
 }
 
 local head = Object.base("Enemy", "Beta Construct Head")
@@ -37,34 +37,34 @@ head.sprite = headSprites.idle
 EliteType.registerPalette(headSprites.palette, head)
 
 head:addCallback("create", function(actor)
-    local actorAc = actor:getAccessor()
-    local data = actor:getData()
-    actorAc.name = "Beta Construct"
---    actorAc.maxhp = data.body:get("maxhp")
---    actorAc.hp = data.body:get("maxhp")
---    actorAc.damage = data.body:get("damage")
-    actorAc.pHmax = 0
-    actorAc.walk_speed_coeff = 0
-    actor:setAnimations{
-        idle = headSprites.idle,
-        jump = headSprites.jump,
-        walk = headSprites.walk,
-        shoot = headSprites.shoot,
-        death = headSprites.death,
-        spawn = headSprites.spawn,
-	palette = headSprites.palette
-    }
---    actorAc.sound_hit = Sound.find("MushHit","vanilla").id
---    actorAc.sound_death = sounds.death.id
---    actor.mask = sprites.mask
-    actorAc.health_tier_threshold = 3
-    actorAc.knockback_cap = 6 * Difficulty.getScaling("hp")
-    actorAc.exp_worth = 0
-    actorAc.can_drop = 0
-    actorAc.can_jump = 0
-    data.shot = false
-    data.angleLocked = false
-    actor.mask = headSprites.mask
+	local actorAc = actor:getAccessor()
+	local data = actor:getData()
+	actorAc.name = "Beta Construct"
+	--    actorAc.maxhp = data.body:get("maxhp")
+	--    actorAc.hp = data.body:get("maxhp")
+	--    actorAc.damage = data.body:get("damage")
+	actorAc.pHmax = 0
+	actorAc.walk_speed_coeff = 0
+	actor:setAnimations {
+		idle = headSprites.idle,
+		jump = headSprites.jump,
+		walk = headSprites.walk,
+		shoot = headSprites.shoot,
+		death = headSprites.death,
+		spawn = headSprites.spawn,
+		palette = headSprites.palette
+	}
+	--    actorAc.sound_hit = Sound.find("MushHit","vanilla").id
+	--    actorAc.sound_death = sounds.death.id
+	--    actor.mask = sprites.mask
+	actorAc.health_tier_threshold = 3
+	actorAc.knockback_cap = 6 * Difficulty.getScaling("hp")
+	actorAc.exp_worth = 0
+	actorAc.can_drop = 0
+	actorAc.can_jump = 0
+	data.shot = false
+	data.angleLocked = false
+	actor.mask = headSprites.mask
 end)
 
 local everyonelol = ParentObject.find("actors")
@@ -72,15 +72,18 @@ head:addCallback("step", function(self)
 	local sD = self:getData()
 	local actorAc = self:getAccessor()
 	self:setAlarm(6, -1)
-	if not sD.body or not sD.body:isValid() then self:delete() return end
+	if not sD.body or not sD.body:isValid() then
+		self:delete()
+		return
+	end
 	if self:get("prefix_type") ~= sD.body:get("prefix_type") then
 		if sD.body:get("prefix_type") == 1 then
 			self:set("prefix_type", sD.body:get("prefix_type"))
 			self:set("elite_type", sD.body:get("elite_type"))
---			local key = sD.body:get("elite_type")
---			self:makeElite(eliteClones[key])
+			--			local key = sD.body:get("elite_type")
+			--			self:makeElite(eliteClones[key])
 		elseif sD.body:get("prefix_type") == 2 then
---			self:makeBlighted(sD.body:getBlighted())
+			--			self:makeBlighted(sD.body:getBlighted())
 			actorAc.prefix_type = 2
 		end
 	end
@@ -101,20 +104,27 @@ head:addCallback("step", function(self)
 	end
 	local targ = Object.findInstance(sD.body:get("target"))
 	if sD.angleLocked == false then
-	if targ ~= nil and targ.y < self.y then
-		local xVar = (math.sign(self.x - targ.x) * (self.x - targ.x))
-		local yVar = (math.sign(self.y - targ.y) * (self.y - targ.y))
-		local c2 = (xVar * xVar) + (yVar * yVar)
-		local h = c2 ^ 0.5
-		local a = xVar
-		self.angle = math.deg(math.asin(a/h))/2 * self.xscale
---		print(self.angle)
-		if math.abs(self.angle) > 75 then
-			self.angle = 75 * self.xscale
+		if targ ~= nil and targ.y < self.y then
+			--		local xVar = (math.sign(self.x - targ.x) * (self.x - targ.x))
+			--		local yVar = (math.sign(self.y - targ.y) * (self.y - targ.y))
+			--		local c2 = (xVar * xVar) + (yVar * yVar)
+			--		local h = c2 ^ 0.5
+			--		local a = xVar
+			--		self.angle = math.deg(math.asin(a/h))/2 * self.xscale
+			local directionToPlayer = {
+				x = targ.x - self.x,
+				y = targ.y - self.y
+			}
+
+			local angleToPlayer = math.deg(math.atan2(-self.xscale * (targ.y - self.y), math.abs(targ.x - self.x)))
+
+			local maxAngle = 75
+			angleToPlayer = math.clamp(angleToPlayer, -maxAngle, maxAngle)
+			self.angle = angleToPlayer
+
+		else
+			self.angle = 0
 		end
-	else
-		self.angle = 0
-	end
 	end
 	if self.sprite == headSprites.shoot and self.subimage <= 2 then
 		sD.shot = false
@@ -127,7 +137,7 @@ head:addCallback("step", function(self)
 		explosion.angle = self.angle
 		explosion.xscale = self.xscale
 		explosion.yscale = self.yscale
---		explosion.alpha = 0.2
+		--		explosion.alpha = 0.2
 		explosion:set("visible", 0)
 		for _, i in ipairs(everyonelol:findAll()) do
 			if i:get("team") ~= self:get("team") and explosion:collidesWith(i, explosion.x, explosion.y) then
@@ -147,23 +157,23 @@ bulletCheck:addCallback("step", function(self)
 end)
 
 local sprites = {
-    idle = Sprite.load("con2Idle", path.."con2Idle", 1, 7, 13),
-    walk = Sprite.load("con2Walk", path.."con2Walk", 6, 7, 20),
-    shoot = Sprite.load("con2Shoot", path.."con2Shoot", 12, 7, 18),
-    log = Sprite.load("con2Shoot_LOG", path.."con2Shoot_LOG", 12, 7, 18),
-    spawn = Sprite.load("con2Spawn", path.."con2Spawn", 15, 8, 16),
-    death = Sprite.load("con2Death", path.."con2Death", 9, 10, 14),
-    mask = Sprite.load("con2Mask", path.."con2Mask", 1, 7, 13),
-    palette = Sprite.load("con2Pal", path.."con2Pal", 1, 0, 0),
-    jump = Sprite.load("con2Jump", path.."con2Jump", 1, 7, 14),
-    portrait = Sprite.load("con2Portrait", path.."con2Portrait", 1, 119, 119)
+	idle = Sprite.load("con2Idle", path .. "con2Idle", 1, 7, 13),
+	walk = Sprite.load("con2Walk", path .. "con2Walk", 6, 7, 20),
+	shoot = Sprite.load("con2Shoot", path .. "con2Shoot", 12, 7, 18),
+	log = Sprite.load("con2Shoot_LOG", path .. "con2Shoot_LOG", 12, 7, 18),
+	spawn = Sprite.load("con2Spawn", path .. "con2Spawn", 15, 8, 16),
+	death = Sprite.load("con2Death", path .. "con2Death", 9, 10, 14),
+	mask = Sprite.load("con2Mask", path .. "con2Mask", 1, 7, 13),
+	palette = Sprite.load("con2Pal", path .. "con2Pal", 1, 0, 0),
+	jump = Sprite.load("con2Jump", path .. "con2Jump", 1, 7, 14),
+	portrait = Sprite.load("con2Portrait", path .. "con2Portrait", 1, 119, 119)
 }
 
 local sounds = {
-    attack = Sound.load( path.."shootCon2"),
-    spawn = Sound.load( path.."spawnCon2"),
-    hit = Sound.load( path.."hitCon2"),
-    death = Sound.load( path.."deathCon2")
+	attack = Sound.load(path .. "shootCon2"),
+	spawn = Sound.load(path .. "spawnCon2"),
+	hit = Sound.load(path .. "hitCon2"),
+	death = Sound.load(path .. "deathCon2")
 }
 
 local con2 = Object.base("EnemyClassic", "Beta Construct2")
@@ -172,32 +182,32 @@ con2.sprite = sprites.idle
 EliteType.registerPalette(sprites.palette, con2)
 
 con2:addCallback("create", function(actor)
-    local actorAc = actor:getAccessor()
-    local data = actor:getData()
-    actorAc.name = "Beta Construct"
-    actorAc.maxhp = 70 * Difficulty.getScaling("hp")
-    actorAc.hp = actorAc.maxhp
-    actorAc.damage = 9 * Difficulty.getScaling("damage")
-    actorAc.pHmax = 1.3
+	local actorAc = actor:getAccessor()
+	local data = actor:getData()
+	actorAc.name = "Beta Construct"
+	actorAc.maxhp = 70 * Difficulty.getScaling("hp")
+	actorAc.hp = actorAc.maxhp
+	actorAc.damage = 9 * Difficulty.getScaling("damage")
+	actorAc.pHmax = 1.3
 	actorAc.walk_speed_coeff = 1.1
-    actor:setAnimations{
-        idle = sprites.idle,
-        jump = sprites.jump,
-        walk = sprites.walk,
-        shoot = sprites.shoot,
-        death = sprites.death,
-	palette = sprites.palette
-    }
-    actorAc.sound_hit = sounds.hit.id
-    actorAc.sound_death = sounds.death.id
-    actor.mask = sprites.mask
-    actorAc.health_tier_threshold = 3
-    actorAc.knockback_cap = 6 * Difficulty.getScaling("hp")
-    actorAc.exp_worth = 5 * Difficulty.getScaling()
-    actorAc.can_drop = 1
-    actorAc.can_jump = 1
-    local mD = head:create(actor.x, actor.y):getData()
-    mD.body = actor
+	actor:setAnimations {
+		idle = sprites.idle,
+		jump = sprites.jump,
+		walk = sprites.walk,
+		shoot = sprites.shoot,
+		death = sprites.death,
+		palette = sprites.palette
+	}
+	actorAc.sound_hit = sounds.hit.id
+	actorAc.sound_death = sounds.death.id
+	actor.mask = sprites.mask
+	actorAc.health_tier_threshold = 3
+	actorAc.knockback_cap = 6 * Difficulty.getScaling("hp")
+	actorAc.exp_worth = 5 * Difficulty.getScaling()
+	actorAc.can_drop = 1
+	actorAc.can_jump = 1
+	local mD = head:create(actor.x, actor.y):getData()
+	mD.body = actor
 end)
 
 Monster.giveAI(con2)
@@ -209,14 +219,17 @@ end)
 Monster.skillCallback(con2, 1, function(actor, relevantFrame)
 	if relevantFrame == 9 then
 		sounds.attack:play(1, 1)
---		actor:fireExplosion(actor.x + actor.xscale * 30, actor.y - 18, 30/19, 5/4, 1)
+		--		actor:fireExplosion(actor.x + actor.xscale * 30, actor.y - 18, 30/19, 5/4, 1)
 	end
 end)
 
---[[con2:addCallback("draw", function(self)
-	graphics.color(Color.RED)
-	graphics.line(self.x, self.y - 4, self.x + (210 * self.xscale), self.y - 4, 1)
-end)]]
+con2:addCallback("draw", function(self)
+
+	-- graphics.color(Color.RED)
+	-- graphics.line(self.x, self.y - 4, self.x + (210 * self.xscale), self.y - 4, 1)
+	
+	
+end)
 
 registercallback("onStep", function()
 	for _, i in ipairs(Object.find("Spawn"):findAll()) do
@@ -243,7 +256,7 @@ card.canBlight = true
 card.type = "classic"
 card.cost = 12
 for _, elite in ipairs(EliteType.findAll("vanilla")) do
-    card.eliteTypes:add(elite)
+	card.eliteTypes:add(elite)
 end
 
 local headCard = MonsterCard.new("con2Head", head)
@@ -263,15 +276,15 @@ end, -100)
 
 local stages
 if modloader.checkMod("Starstorm") then
-stages = {
-	Stage.find("Magma Barracks"),
-	Stage.find("Temple of the Elders")
+	stages = {
+		Stage.find("Magma Barracks"),
+		Stage.find("Temple of the Elders")
 	}
 else
-stages = {
-	Stage.find("Magma Barracks"),
-	Stage.find("Temple of the Elders")
-}
+	stages = {
+		Stage.find("Magma Barracks"),
+		Stage.find("Temple of the Elders")
+	}
 end
 
 for _, stage in ipairs(stages) do
@@ -280,14 +293,14 @@ end
 
 local stages2
 if modloader.checkMod("Starstorm") then
-stages2 = {
-	Stage.find("Damp Caverns"),
-	Stage.find("Uncharted Mountain")
+	stages2 = {
+		Stage.find("Damp Caverns"),
+		Stage.find("Uncharted Mountain")
 	}
 else
-stages2 = {
-	Stage.find("Damp Caverns")
-}
+	stages2 = {
+		Stage.find("Damp Caverns")
+	}
 end
 
 registercallback("onGameStart", function()
@@ -313,11 +326,12 @@ registercallback("onGameEnd", function()
 	end
 end)
 
-local monsLog = MonsterLog.new("Beta Construct2")
+local monsLog = MonsterLog.find("Beta Construct2")
 MonsterLog.map[con2] = monsLog
 
 monsLog.displayName = "Beta Construct"
-monsLog.story = "Malformed constructs whizz past me, shaky legs carrying their crumbling bodies at speeds too reckless to be safe, for it or myself. Their shield-like plates are covered in cracks and blemishes, no doubt from running into obstacles. The imperfections in their shield lend themselves to combat; the energy blasts they launch are chaotic and dispersed like light through a prism."
+monsLog.story =
+"Malformed constructs whizz past me, shaky legs carrying their crumbling bodies at speeds too reckless to be safe, for it or myself. Their shield-like plates are covered in cracks and blemishes, no doubt from running into obstacles. The imperfections in their shield lend themselves to combat; the energy blasts they launch are chaotic and dispersed like light through a prism."
 monsLog.statHP = 70
 monsLog.statDamage = 12
 monsLog.statSpeed = 1.3
