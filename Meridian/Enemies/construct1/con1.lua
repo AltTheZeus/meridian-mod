84329d05a6f0eeded90c829b3b427b65767d11b5
retroPacifist <66305851+retroPacifist@users.noreply.github.com>
2024-10-10 23:53:08 +1000

    Update con1.lua
    disabled beta construct a being blighted

--- Meridian/Enemies/construct1/con1.lua
+++ Meridian/Enemies/construct1/con1.lua
@@ -89,7 +89,7 @@ local card = MonsterCard.new("con1", con1)
 card.sprite = sprites.idle
 card.sprite = sprites.spawn
 card.sound = sounds.spawn
-card.canBlight = true
+card.canBlight = false
 card.type = "classic"
 card.cost = 12
 for _, elite in ipairs(EliteType.findAll("vanilla")) do
