fe6f207de5f5dd329a0cbc2f352a3e3595fc3d6c
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2024-12-18 17:55:44 +0100

    TRYAGAIN211 Changes - Molding Elite
    the mold consumes

--- Meridian/Elites/evoke.lua
+++ Meridian/Elites/evoke.lua
@@ -29,22 +29,24 @@ eliteBabyBlessed.palette = sprPalBabyBlessed
 registercallback("onEliteInit", function(actor)
 	local aD = actor:getData()
 	if actor:get("elite_type") == ID or actor:get("elite_type") == bID then
-		aD.pointTimer = 0
-		aD.points = 0
-		local enemyOptions = Stage.getCurrentStage().enemies:toTable()
-		if #enemyOptions < 1 then
-			aD.card = MonsterCard.find("Sand Crab")
-		else
-			local failsafe = 0
-			repeat
-				failsafe = failsafe + 1
-				aD.card = table.random(enemyOptions)
-			until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
-			if failsafe >= 30 then
+		if net.host then
+			aD.pointTimer = 0
+			aD.points = 0
+			local enemyOptions = Stage.getCurrentStage().enemies:toTable()
+			if #enemyOptions < 1 then
 				aD.card = MonsterCard.find("Sand Crab")
+			else
+				local failsafe = 0
+				repeat
+					failsafe = failsafe + 1
+					aD.card = table.random(enemyOptions)
+				until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
+				if failsafe >= 30 then
+					aD.card = MonsterCard.find("Sand Crab")
+				end
 			end
+			aD.timer = aD.card.cost
 		end
-		aD.timer = aD.card.cost
 		aD.minionCount = 0
 		aD.eliteVar = 1
 	end
@@ -52,188 +54,184 @@ end)
 
 local enemies = ParentObject.find("enemies")
 
+spawnBabyFunc = setFunc(function(actor, parent, eID, cardID)
+	local eliteType = EliteType.fromID(eID)
+	local monsterCard = MonsterCard.fromID(cardID)
+	local actorData = actor:getData()
+	monsterCard.eliteTypes:add(eliteType)
+	actorData.evoker = parent.id
+	parent:getData().minionCount = parent:getData().minionCount + 1
+	actor:makeElite(eliteType)
+	monsterCard.eliteTypes:remove(eliteType)
+	actor:set("maxhp", (math.round(((actor:get("maxhp") / 2.6) * 0.75))))
+	actor:set("hp", actor:get("maxhp"))
+	actor:set("damage", (math.round(((actor:get("damage") / 1.7) * 0.75))))
+	actor:set("exp_worth", (math.round(((actor:get("exp_worth") / 2) * 0.5))))
+	actor:set("cdr", actor:get("cdr") - 0.3)
+	actor:set("knockback_cap", actor:get("knockback_cap") / 3)
+	actorData.terezi = 1
+	if eID == bIDb then
+		actorData.sparkleCD = 15
+	end
+end)
+
 registercallback("onStep", function()
-	if misc.getTimeStop() == 0 then
-	for _, i in ipairs(enemies:findMatching("elite_type", ID)) do
-		local aD = i:getData()
-		if aD.eliteVar == 1 then
-		if aD.minionCount < 3 then
-			aD.pointTimer = aD.pointTimer + 1
-		end
-		if aD.pointTimer >= 60 then
-			aD.points = aD.points + 1 + misc.director:get("stages_passed")
-			aD.pointTimer = 0
-		end
-		if aD.points >= aD.timer and aD.minionCount < 3 then
-local heightLevel = 1
+	if net.host and misc.getTimeStop() == 0 then
+		for _, i in ipairs(enemies:findMatching("elite_type", ID)) do
+			local aD = i:getData()
+			if aD.eliteVar == 1 then
+			if aD.minionCount < 3 then
+				aD.pointTimer = aD.pointTimer + 1
+			end
+			if aD.pointTimer >= 60 then
+				aD.points = aD.points + 1 + misc.director:get("stages_passed")
+				aD.pointTimer = 0
+			end
+			if aD.points >= aD.timer and aD.minionCount < 3 then
+				local heightLevel = 1
 
-local groundLevel = 1
-local groundC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y + 20)
-if groundC1 then groundC1 = groundC1.y end
-local groundC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y + 20)
-if groundC2 then groundC2 = groundC2.y end
-if groundC1 and groundC2 then
-	if groundC2 < groundC1 then
-		groundLevel = groundC2
-	else
-		groundLevel = groundC1
-	end
-elseif groundC1 then
-	groundLevel = groundC1
-elseif groundC2 then
-	groundLevel = groundC2
-end
+				local groundLevel = 1
+				local groundC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y + 20)
+				if groundC1 then groundC1 = groundC1.y end
+					local groundC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y + 20)
+					if groundC2 then groundC2 = groundC2.y end
+					if groundC1 and groundC2 then
+						if groundC2 < groundC1 then
+							groundLevel = groundC2
+						else
+							groundLevel = groundC1
+						end
+					elseif groundC1 then
+						groundLevel = groundC1
+					elseif groundC2 then
+						groundLevel = groundC2
+					end
 
-local ceilLevel = 1
-local ceilC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y - 50)
-if ceilC1 then
-	ceilC1 = ceilC1.y + (ceilC1:get("height_box") * 16)
-end
-local ceilC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y - 50)
-if ceilC2 then
-	ceilC2 = ceilC2.y + (ceilC2:get("height_box") * 16)
-end
-if ceilC1 and ceilC2 then
-	if ceilC2 > ceilC1 then
-		ceilLevel = ceilC2
-	else
-		ceilLevel = ceilC1
-	end
-elseif ceilC1 then
-	ceilLevel = ceilC1
-elseif ceilC2 then
-	ceilLevel = ceilC2
-end
+					local ceilLevel = 1
+					local ceilC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y - 50)
+					if ceilC1 then
+						ceilC1 = ceilC1.y + (ceilC1:get("height_box") * 16)
+					end
+					local ceilC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y - 50)
+					if ceilC2 then
+						ceilC2 = ceilC2.y + (ceilC2:get("height_box") * 16)
+					end
+					if ceilC1 and ceilC2 then
+						if ceilC2 > ceilC1 then
+							ceilLevel = ceilC2
+						else
+							ceilLevel = ceilC1
+						end
+					elseif ceilC1 then
+						ceilLevel = ceilC1
+					elseif ceilC2 then
+						ceilLevel = ceilC2
+					end
 
-if ceilLevel and groundLevel then
-	heightLevel = groundLevel - ceilLevel
-end
-if aD.card.object.sprite.height < heightLevel then
---			print("aaaaaaaah")
---			print("Ground at Y"..groundLevel)
---			print("Ceiling at Y"..ceilLevel)
---			print(heightLevel)
-			aD.points = aD.points - aD.timer
-			aD.card.eliteTypes:add(eliteBaby)
-			local newGuy = aD.card.object:create(i.x, groundLevel - aD.card.object.sprite.height + aD.card.object.sprite.yorigin)
-			local nD = newGuy:getData()
-			nD.evoker = i.id
-			aD.minionCount = aD.minionCount + 1
-			newGuy:makeElite(eliteBaby)
-			aD.card.eliteTypes:remove(eliteBaby)
-				newGuy:set("maxhp", (math.round(((newGuy:get("maxhp") / 2.6) * 0.75))))
-				newGuy:set("hp", newGuy:get("maxhp"))
-				newGuy:set("damage", (math.round(((newGuy:get("damage") / 1.7) * 0.75))))
-				newGuy:set("exp_worth", (math.round(((newGuy:get("exp_worth") / 2) * 0.5))))
-				newGuy:set("cdr", newGuy:get("cdr") - 0.3)
-				nD.terezi = 1
-			local enemyOptions = Stage.getCurrentStage().enemies:toTable()
-			if #enemyOptions < 1 then
-				aD.card = MonsterCard.find("Sand Crab")
-			else
-				local failsafe = 0
-				repeat
-					failsafe = failsafe + 1
-					aD.card = table.random(enemyOptions)
-				until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
-				if failsafe >= 30 then
-					aD.card = MonsterCard.find("Sand Crab")
+					if ceilLevel and groundLevel then
+						heightLevel = groundLevel - ceilLevel
+					end
+					if aD.card.object.sprite.height < heightLevel then
+						--print("aaaaaaaah")
+						--print("Ground at Y"..groundLevel)
+						--print("Ceiling at Y"..ceilLevel)
+						--print(heightLevel)
+						aD.points = aD.points - aD.timer
+						local newGuy = createSynced(aD.card.object, i.x, groundLevel - aD.card.object.sprite.height + aD.card.object.sprite.yorigin, spawnBabyFunc, i, IDb, aD.card.id)
+						local enemyOptions = Stage.getCurrentStage().enemies:toTable()
+						if #enemyOptions < 1 then
+							aD.card = MonsterCard.find("Sand Crab")
+						else
+							local failsafe = 0
+							repeat
+								failsafe = failsafe + 1
+								aD.card = table.random(enemyOptions)
+							until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
+							if failsafe >= 30 then
+								aD.card = MonsterCard.find("Sand Crab")
+							end
+						end
+						aD.timer = aD.card.cost
+					end
 				end
 			end
-			aD.timer = aD.card.cost
 		end
-		end
-	end
-end
-	for _, i in ipairs(enemies:findMatching("elite_type", bID)) do
-		local aD = i:getData()
-		if aD.eliteVar == 1 then
-		if aD.minionCount < 4 then
-			aD.pointTimer = aD.pointTimer + 1
-		end
-		if aD.pointTimer >= 60 then
-			aD.points = aD.points + 1 + misc.director:get("stages_passed")
-			aD.pointTimer = 0
-		end
-		if aD.points >= aD.timer and aD.minionCount < 4 then
-local heightLevel = 1
+		for _, i in ipairs(enemies:findMatching("elite_type", bID)) do
+				local aD = i:getData()
+				if aD.eliteVar == 1 then
+					if aD.minionCount < 4 then
+						aD.pointTimer = aD.pointTimer + 1
+					end
+					if aD.pointTimer >= 60 then
+						aD.points = aD.points + 1 + misc.director:get("stages_passed")
+						aD.pointTimer = 0
+					end
+					if aD.points >= aD.timer and aD.minionCount < 4 then
+						local heightLevel = 1
+						
+						local groundLevel = 1
+						local groundC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y + 20)
+						if groundC1 then groundC1 = groundC1.y end
+						local groundC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y + 20)
+						if groundC2 then groundC2 = groundC2.y end
+						if groundC1 and groundC2 then
+							if groundC2 < groundC1 then
+								groundLevel = groundC2
+							else
+								groundLevel = groundC1
+							end
+					elseif groundC1 then
+						groundLevel = groundC1
+					elseif groundC2 then
+						groundLevel = groundC2
+					end
+					
+					local ceilLevel = 1
+					local ceilC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y - 50)
+					if ceilC1 then
+						ceilC1 = ceilC1.y + (ceilC1:get("height_box") * 16)
+					end
+					local ceilC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y - 50)
+					if ceilC2 then
+						ceilC2 = ceilC2.y + (ceilC2:get("height_box") * 16)
+					end
+					if ceilC1 and ceilC2 then
+						if ceilC2 > ceilC1 then
+							ceilLevel = ceilC2
+						else
+							ceilLevel = ceilC1
+						end
+					elseif ceilC1 then
+						ceilLevel = ceilC1
+					elseif ceilC2 then
+						ceilLevel = ceilC2
+					end
 
-local groundLevel = 1
-local groundC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y + 20)
-if groundC1 then groundC1 = groundC1.y end
-local groundC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y + 20)
-if groundC2 then groundC2 = groundC2.y end
-if groundC1 and groundC2 then
-	if groundC2 < groundC1 then
-		groundLevel = groundC2
-	else
-		groundLevel = groundC1
-	end
-elseif groundC1 then
-	groundLevel = groundC1
-elseif groundC2 then
-	groundLevel = groundC2
-end
-
-local ceilLevel = 1
-local ceilC1 = Object.find("B"):findLine(i.x, i.y, i.x, i.y - 50)
-if ceilC1 then
-	ceilC1 = ceilC1.y + (ceilC1:get("height_box") * 16)
-end
-local ceilC2 = Object.find("BNoSpawn"):findLine(i.x, i.y, i.x, i.y - 50)
-if ceilC2 then
-	ceilC2 = ceilC2.y + (ceilC2:get("height_box") * 16)
-end
-if ceilC1 and ceilC2 then
-	if ceilC2 > ceilC1 then
-		ceilLevel = ceilC2
-	else
-		ceilLevel = ceilC1
-	end
-elseif ceilC1 then
-	ceilLevel = ceilC1
-elseif ceilC2 then
-	ceilLevel = ceilC2
-end
-
-if ceilLevel and groundLevel then
-	heightLevel = groundLevel - ceilLevel
-end
-if aD.card.object.sprite.height < heightLevel then
-			aD.points = aD.points - aD.timer
-			aD.card.eliteTypes:add(eliteBabyBlessed)
-			local newGuy = aD.card.object:create(i.x, groundLevel - aD.card.object.sprite.height + aD.card.object.sprite.yorigin)
-			local nD = newGuy:getData()
-			nD.evoker = i.id
-			aD.minionCount = aD.minionCount + 1
-			newGuy:makeElite(eliteBabyBlessed)
-			aD.card.eliteTypes:remove(eliteBabyBlessed)
-				newGuy:set("maxhp", (math.round(((newGuy:get("maxhp") / 2.6) * 0.75))))
-				newGuy:set("hp", newGuy:get("maxhp"))
-				newGuy:set("damage", (math.round(((newGuy:get("damage") / 1.7) * 0.75))))
-				newGuy:set("exp_worth", (math.round(((newGuy:get("exp_worth") / 2) * 0.5))))
-				newGuy:set("cdr", newGuy:get("cdr") - 0.3)
-				nD.terezi = 1
-				nD.sparkleCD = 15
-			local enemyOptions = Stage.getCurrentStage().enemies:toTable()
-			if #enemyOptions < 1 then
-				aD.card = MonsterCard.find("Sand Crab")
-			else
-				local failsafe = 0
-				repeat
-					failsafe = failsafe + 1
-					aD.card = table.random(enemyOptions)
-				until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
-				if failsafe >= 30 then
-					aD.card = MonsterCard.find("Sand Crab")
+					if ceilLevel and groundLevel then
+						heightLevel = groundLevel - ceilLevel
+					end
+					if aD.card.object.sprite.height < heightLevel then
+						aD.points = aD.points - aD.timer
+						local newGuy = createSynced(aD.card.object, i.x, groundLevel - aD.card.object.sprite.height + aD.card.object.sprite.yorigin, spawnBabyFunc, i, bIDb, aD.card.id)
+						local enemyOptions = Stage.getCurrentStage().enemies:toTable()
+						if #enemyOptions < 1 then
+							aD.card = MonsterCard.find("Sand Crab")
+						else
+							local failsafe = 0
+							repeat
+								failsafe = failsafe + 1
+								aD.card = table.random(enemyOptions)
+							until (aD.card.cost < 500 and aD.card.type == "classic") or failsafe >= 30
+							if failsafe >= 30 then
+								aD.card = MonsterCard.find("Sand Crab")
+							end
+						end
+						aD.timer = aD.card.cost
+					end
 				end
 			end
-			aD.timer = aD.card.cost
-		end
 		end
 	end
-	end
-end
 end)
 
 registercallback("onNPCDeath", function(npc)
@@ -275,7 +273,7 @@ registercallback("onStep", function()
 			i:delete()
 		end
 	end
-			for _, i in ipairs(enemies:findMatching("elite_type", bIDb)) do
+	for _, i in ipairs(enemies:findMatching("elite_type", bIDb)) do
 		local aD = i:getData()
 		if aD.sparkleCD and aD.sparkleCD > 0 then aD.sparkleCD = aD.sparkleCD - 1 end
 		if aD.sparkleCD and aD.sparkleCD <= 0 then
