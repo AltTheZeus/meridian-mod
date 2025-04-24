6e47436cbfa081d66aa75c41b660043e94364fb2
Altzeus <88247068+AltTheZeus@users.noreply.github.com>
2025-04-24 22:46:38 +0300

    Commenting out spawns

--- Meridian/Enemies/construct1/con1.lua
+++ Meridian/Enemies/construct1/con1.lua
@@ -96,7 +96,7 @@ for _, elite in ipairs(EliteType.findAll("vanilla")) do
     card.eliteTypes:add(elite)
 end
 
-local stages
+--[[local stages
 if modloader.checkMod("Starstorm") then
 stages = {
 	Stage.find("Sky Meadow"),
@@ -146,7 +146,7 @@ registercallback("onGameEnd", function()
 	for _, stage in ipairs(stages2) do
 		stage.enemies:remove(card)
 	end
-end)
+end)]]
 
 local monsLog = MonsterLog.find("Beta Construct1")
 MonsterLog.map[con1] = monsLog
